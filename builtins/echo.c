a0b821bcca64fd99a5614fb0e62f895bb79ec590
cyanide319 <tristan-b@live.ca>
2023-01-25 14:40:50 -0500

    Signal handlers....Working?

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,43 +6,65 @@
 /*   By: tbeaudoi <tbeaudoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 05:39:33 by iamongeo          #+#    #+#             */
-/*   Updated: 2023/01/19 19:53:23 by tbeaudoi         ###   ########.fr       */
+/*   Updated: 2023/01/23 23:25:42 by tbeaudoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	parse_echo_cmd(char *cmd, char ***strs)
-{
-	(void)cmd;
-	(void)strs;
-	return (0);
-}
+// static int	parse_echo_cmd(char *cmd, char ***strs)
+// {
+// 	(void)cmd;
+// 	(void)strs;
+// 	return (0);
+// }
 
 	// TODO : parse cmd to substitute environment variables with 
 	//		substitute_env_vars(t_msh *msh, char *line, char **ret)
 	//		then print without quotes chars.
 	//		ps : first word in cmd is 'echo'.
-int	msh_builtin_echo(t_job *job, char *cmd)//char **strs)
+// int	msh_builtin_echo(t_job *job, char *cmd)//char **strs)
+// {
+// 	int		new_line;
+// 	char	**strs;
+
+// 	(void)job;
+// 	if (parse_echo_cmd(cmd, &strs) < 0)
+// 		return (-1);
+// 	new_line = 1;
+// 	while (*strs)
+// 	{
+// 		if (ft_strncmp(*strs, "-n", 2) == 0)
+// 			new_line = 0;
+// 		else if (*(strs + 1))
+// 			ft_printf("%s ", *strs);
+// 		else
+// 			ft_putstr(*strs);
+// 		strs++;
+// 	}
+// 	if (new_line)
+// 		write(1, "\n", 1);
+// 	return (0);
+// }
+
+int	msh_builtin_echo(t_job *job, char *cmd)
 {
-	int		new_line;
-	char	**strs;
+	int		j;
 
 	(void)job;
-	if (parse_echo_cmd(cmd, &strs) < 0)
-		return (-1);
-	new_line = 1;
-	while (*strs)
+	j = 1;
+	if (!cmd[1])
+		printf("\n");
+	if (!ft_strncmp(&cmd[1], "-n", 3))
+		j++;
+	while (cmd[j])
 	{
-		if (ft_strncmp(*strs, "-n", 2) == 0)
-			new_line = 0;
-		else if (*(strs + 1))
-			ft_printf("%s ", *strs);
-		else
-			ft_putstr(*strs);
-		strs++;
+		printf("%c", cmd[j]);
+		if (cmd[j + 1])
+			printf(" ");
+		j++;
 	}
-	if (new_line)
-		write(1, "\n", 1);
-	return (0);
+	if (ft_strncmp(&cmd[1], "-n", 3))
+		printf("\n");
+	return (1);
 }
--- src/sig_handlers.c
+++ src/sig_handlers.c
@@ -6,7 +6,7 @@
 /*   By: tbeaudoi <tbeaudoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 20:01:38 by tbeaudoi          #+#    #+#             */
-/*   Updated: 2023/01/19 10:48:41 by tbeaudoi         ###   ########.fr       */
+/*   Updated: 2023/01/23 23:40:17 by tbeaudoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,16 @@ void	sig_handler(int signum, siginfo_t *info, void *context)
 	}
 }
 
+void	sig_handler_child(int signum, siginfo_t *info, void *context)
+{
+	(void)context;
+	(void)info;
+	if (signum == SIGQUIT)
+		(void) SIGQUIT;
+	if (signum == SIGINT)
+		(void) SIGINT;
+}
+
 void	handlers_control(t_msh *msh)
 {
 	struct sigaction	sa;
@@ -37,8 +47,10 @@ void	handlers_control(t_msh *msh)
 		sa.sa_sigaction = &sig_handler;
 		sigaction(SIGINT, &sa, NULL);
 	}
-	// if (msh->exec_status == EXEC_MODE)
-	// {
-		
-	// }
+	if (msh->exec_status == EXEC_MODE)
+	{
+		sa.sa_sigaction = &sig_handler_child;
+		sigaction(SIGINT, &sa, NULL);
+		sigaction(SIGQUIT, &sa, NULL);
+	}
 }
